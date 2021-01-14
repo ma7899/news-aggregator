89f136cc0ed6efc6054d26abb2e8ebf0c34f3674
ma7899 <skymage78@gmail.com>
2021-01-14 12:15:23 +0330

    basic structor

---
+++ src/actions/newsActionCreator.js
@@ -0,0 +1,8 @@
+import { createAction } from 'redux-actions';
+
+import { ActionTypes } from '../constants/AppConstants';
+
+const {
+
+} = ActionTypes;
+
---
+++ src/reducers/newsReducer.js
@@ -0,0 +1,15 @@
+import { handleActions } from 'redux-actions';
+
+import { ActionTypes } from '../constants/AppConstants';
+
+const {
+
+} = ActionTypes;
+
+const defaultState = {
+
+};
+
+export default handleActions({
+
+}, defaultState);
